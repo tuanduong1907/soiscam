731bd9927a86b0d6063ff6001b3333b07edfa96f
tuanduong1907 <91052899+tuanduong1907@users.noreply.github.com>
2022-02-06 23:40:55 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,8 +39,9 @@ function showNotifi(e) {
 // event
 document.body.addEventListener('click', (e) =>{
     const modal =  document.querySelector('.modal')
-    if(e.target.matches('.active')) {
-        menu.classList.remove('active')
+    const isActive = document.querySelector('.is-active')
+    if(isActive) {
+        menu.classList.remove('is-active')
     }else if(e.target.matches('.modal')) {
         modal.parentNode.removeChild(e.target)
     }else if(e.target.matches('.modal-close')) {
@@ -50,7 +51,7 @@ document.body.addEventListener('click', (e) =>{
 menu.addEventListener('click',stopPropagation)
 
 menuBtn.addEventListener('click', (e) =>{
-    menu.classList.toggle('active')
+    menu.classList.toggle('is-active')
     stopPropagation(e)
 })
 
